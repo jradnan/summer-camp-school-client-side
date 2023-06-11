a934571b430a06f3a2a792aa3196f244b5568674
jradnan <mohd.yasin085208@gmail.com>
2023-06-11 14:20:48 +0600

    loading spinner added

--- src/components/Classes/Classes.jsx
+++ src/components/Classes/Classes.jsx
@@ -4,20 +4,26 @@ import AllClasses from "./AllClasses";
 
 const Classes = () => {
     const [courses, setCourses] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         fetch('https://summer-camp-school-server-afridimhrj10-gmailcom.vercel.app/courses')
             .then(res => res.json())
             .then(data => {
                 setCourses(data[0].classes);
+                setIsLoading(false);
             })
     }, [])
     return (
         <div>
-            <div className="courses  p-10 lg:p-20 md:p-20 lg:mx-12 lg:gap-6  grid md:grid-cols-2 lg:grid-cols-3">
-                {
-                    courses.map(course => <AllClasses course={course} key={course.id}></AllClasses>)
-                }
-            </div>
+            {isLoading ? (
+                <div className="lg:text-[150px] text-[30px] md:text-[140px] mt-48  flex items-center justify-center">L<span><img src="https://i.ibb.co/B23wWVn/images-removebg-preview.png" alt="" /></span>ading...</div>
+            ) : (
+                <div className="courses p-10 lg:p-20 md:p-20 lg:mx-12 lg:gap-6  grid md:grid-cols-2 lg:grid-cols-3">
+                    {courses.map(course => (
+                        <AllClasses key={course.id} course={course} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
--- src/components/Intructors/Instructors.jsx
+++ src/components/Intructors/Instructors.jsx
@@ -3,22 +3,28 @@ import AllInstructor from "./AllInstructor";
 
 const Instructors = () => {
     const [allInstructors, setAllInstructors] = useState([]);
-    useEffect(()=>{
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
         fetch('https://summer-camp-school-server-afridimhrj10-gmailcom.vercel.app/instructors')
-        .then(res => res.json())
-        .then(data =>{
-            setAllInstructors(data[0].instructors);
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setAllInstructors(data[0].instructors);
+                setIsLoading(false);
+            })
+    }, [])
 
     console.log(allInstructors);
     return (
         <div>
-           <div className="courses  p-10 lg:p-20 md:p-20 lg:mx-12 lg:gap-6  grid md:grid-cols-2 lg:grid-cols-3">
-                {
-                    allInstructors.map(instructor => <AllInstructor key={instructor.id} instructor={instructor}></AllInstructor>)
-                }
-            </div>
+            {isLoading ? (
+                <div className="lg:text-[150px] md:text-[140px] text-[30px] mt-48  flex items-center justify-center">L<span><img src="https://i.ibb.co/B23wWVn/images-removebg-preview.png" alt="" /></span>ading...</div>
+            ) : (
+                <div className="courses p-10 lg:p-20 md:p-20 lg:mx-12 lg:gap-6  grid md:grid-cols-2 lg:grid-cols-3">
+                    {allInstructors.map(instructor => (
+                        <AllInstructor key={instructor.id} instructor={instructor} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
